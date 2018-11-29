7f3a78d5cfff722a62b6ffdf06e66994b989ab2a
Gary Liang <gary@arkade.com.au>
2018-11-29 22:44:23 +1100

    up

--- src/App.js
+++ src/App.js
@@ -1,28 +1,102 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Util} from './Util';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+
+        this.util = new Util();
+
+        this.state = {
+            vs5_num: '',
+            mb11_num: '',
+            cf_num: '',
+        }
+    }
+
+    handleVs5(e) {
+        let value = e.target.value;
+
+        this.setState({
+            vs5_num: value
+        });
+    }
+
+    handleMb11(e) {
+        let value = e.target.value;
+
+        this.setState({
+            mb11_num: value
+        });
+    }
+
+    handleCf(e) {
+        let value = e.target.value;
+
+        this.setState({
+            cf_num: value
+        });
+    }
+
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        let {vs5_num, mb11_num, cf_num} = this.state;
+
+        this.util.testme();
+    }
+
+    render() {
+        return (
+            <div className="myapp">
+                <form onSubmit={this.handleSubmit.bind(this)}>
+                    <div className='row'>
+                        <div className='col xs-6'>
+                            VS5:
+                        </div>
+                        <div className='col xs-6'>
+                            <input
+                                key='vs5_num'
+                                value={this.state.vs5_num}
+                                onChange={this.handleVs5.bind(this)}
+                            />
+                        </div>
+                    </div>
+
+                    <div className='row'>
+                        <div className='col xs-6'>
+                            MB11:
+                        </div>
+                        <div className='col xs-6'>
+                            <input
+                                key='mb11_num'
+                                value={this.state.mb11_num}
+                                onChange={this.handleMb11.bind(this)}
+                            />
+                        </div>
+                    </div>
+
+                    <div className='row'>
+                        <div className='col xs-6'>
+                            CF:
+                        </div>
+                        <div className='col xs-6'>
+                            <input
+                                key='cf_num'
+                                value={this.state.cf_num}
+                                onChange={this.handleCf.bind(this)}
+                            />
+                        </div>
+                    </div>
+
+                    <button>Submit</button>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Util.js
@@ -0,0 +1,11 @@
+export class Util {
+
+    constructor() {
+
+    }
+
+    testme() {
+        console.log('sdf');
+    }
+
+}
